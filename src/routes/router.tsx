85033731c3e3c773783319f9ac9749bfebbc0bd1
chenterphai <chenterphai@gmail.com>
2026-06-26 14:09:47 +0700

    Install needed components and setup vercel

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,3 +1,16 @@
+import Dashboard from '@/app/dashboard/dashboard';
+import App from '@/app/shell/app';
 import { createBrowserRouter } from 'react-router';
 
-export const router = createBrowserRouter([]);
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        Component: Dashboard,
+      },
+    ],
+  },
+]);
